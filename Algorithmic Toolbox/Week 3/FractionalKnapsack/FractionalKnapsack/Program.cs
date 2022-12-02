06232055ea4a0fe17f1d4670dfe83623f98bcaa6
Adnan Niloy <apache_adnan1313@hotmail.com>
2022-12-02 23:48:28 +0600

    Week 3

---
+++ Algorithmic Toolbox/Week 3/FractionalKnapsack/FractionalKnapsack/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FractionalKnapsack
+{
+    internal class Program
+    {
+        static double GetOptimal(int capacity, List<Knapsack> sack)
+        {
+            sack = sack.OrderByDescending(c => c.Factor).ToList();
+            double result = 0;
+            var size = sack.Count;
+
+            for (var i = 0; i < size; i++)
+            {
+                if (sack[i].Weight <= 0)
+                    continue;
+
+                if (sack[i].Weight <= capacity)
+                {
+                    result += sack[i].Cost;
+                    capacity -= sack[i].Weight;
+                }
+                else
+                {
+                    result += (double)sack[i].Cost / sack[i].Weight * capacity;
+                    capacity -= capacity;
+                }
+
+                if (capacity <= 0)
+                    break;
+
+            }
+
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var tokens = input.Split(' ');
+            var n = int.Parse(tokens[0]);
+            var c = int.Parse(tokens[1]);
+
+            var sack = new List<Knapsack>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+                var items = line.Split(' ');
+
+                var item = new Knapsack
+                {
+                    Cost = int.Parse(items[0]),
+                    Weight = int.Parse(items[1])
+                };
+
+                item.Factor = (double)item.Cost / item.Weight;
+
+                sack.Add(item);
+            }
+
+
+            Console.WriteLine(GetOptimal(c, sack));
+
+        }
+    }
+
+    public class Knapsack
+    {
+        public int Cost { get; set; }
+
+        public int Weight { get; set; }
+
+        public double Factor { get; set; }
+    }
+}
---
+++ Algorithmic Toolbox/Week 3/MoneyChange/MoneyChange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MoneyChange
+{
+    internal class Program
+    {
+        private static int Change(int m)
+        {
+            var result = 0;
+
+            while (m > 0)
+            {
+                if (m >= 10)
+                    m -= 10;
+                else if (m >= 5)
+                    m -= 5;
+                else
+                    m -= 1;
+
+                result++;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            var input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Change(input));
+        }
+    }
+}
